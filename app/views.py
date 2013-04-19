0e57b3cc3972199f954ecc6b4df9d6a68077931d
Patrick Huck <phuck@lbl.gov>
2013-04-19 16:49:55 -0700

    test db in index

--- app/views.py
+++ app/views.py
@@ -1,18 +1,19 @@
 from flask import render_template
-from app import app, models
+from app import app, db
+from app.models import Post
 import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-  posts = [ # fake
-           models.Post(title = 'first post',
-                body = 'lorem ipsum',
-                timestamp = datetime.datetime.utcnow()
-               ),
-           models.Post(title = 'second post',
-                body = 'blafasl',
-                timestamp = datetime.datetime.utcnow()
-               )
-          ] 
+  p = Post(title = 'first post', body = 'lorem ipsum',
+           timestamp = datetime.datetime.utcnow()
+          )
+  db.session.add(p)
+  p = Post(title = 'second post', body = 'blafasl',
+           timestamp = datetime.datetime.utcnow()
+          )
+  db.session.add(p)
+  db.session.commit()
+  posts = Post.query.all()
   return render_template("index.html", posts = posts)
