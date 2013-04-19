8ba933a1bc9f4b808be30fff36d806fa3242610c
Patrick Huck <phuck@lbl.gov>
2013-04-19 16:06:50 -0700

    use Post model for fake

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 from flask import render_template
-from app import app
+from app import app, models
+import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
   posts = [ # fake
-           { 'title': 'first post', 'body': 'lorem ipsum' },
-           { 'title': 'second post', 'body': 'blafasl' }
+           models.Post(title = 'first post',
+                body = 'lorem ipsum',
+                timestamp = datetime.datetime.utcnow()
+               ),
+           models.Post(title = 'second post',
+                body = 'blafasl',
+                timestamp = datetime.datetime.utcnow()
+               )
           ] 
   return render_template("index.html", posts = posts)
