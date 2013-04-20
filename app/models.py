32ecb58ac11b638ffe023a6ebe3044f37ed0f9c0
Patrick Huck <phuck@lbl.gov>
2013-04-20 00:45:51 -0700

    models.py: shorten body, ctime

--- app/models.py
+++ app/models.py
@@ -3,7 +3,9 @@ from app import db
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   title = db.Column(db.String(120))
-  body = db.Column(db.String(1000), unique=True)
+  body = db.Column(db.String(100), unique=True)
   timestamp = db.Column(db.DateTime)
   def __repr__(self):
-    return '<Id %r, Title %r, Body %r>' % (self.id, self.title, self.body)
+    return '<Id %r, Title %r, Body %r, Date %r>' % (
+      self.id, self.title, self.body, self.timestamp.ctime()
+    )
