119aa8a9c54bbcea932ed5f87acc8d38cfa9481e
Patrick Huck <phuck@lbl.gov>
2013-04-20 03:29:39 -0700

    implement archive

--- app/views.py
+++ app/views.py
@@ -6,17 +6,30 @@ from asciidocapi import AsciiDocAPI
 asciidoc = AsciiDocAPI()
 asciidoc.options('--no-header-footer')
 
+def run_asciidoc(p):
+  outfile = StringIO.StringIO()
+  asciidoc.execute(str(p.body), outfile, backend = 'html4')
+  return outfile.getvalue()
+
 @app.route('/')
 @app.route('/index')
 @app.route('/index/<int:page>')
 def index(page = 1):
   posts = Post.query.paginate(page, 1, False) # returns Paginate object
   for p in posts.items:
-    outfile = StringIO.StringIO()
-    asciidoc.execute(str(p.body), outfile, backend = 'html4')
-    p.body = outfile.getvalue()
+    p.body = run_asciidoc(p)
   return render_template("index.html", posts = posts)
 
 @app.route('/about')
 def about():
   return render_template("about.html")
+
+@app.route('/archive')
+def archive():
+  return render_template("archive.html", posts = Post.query.all())
+
+@app.route('/archive/<int:id>')
+def post(id = 1):
+  post = Post.query.get(id)
+  post.body = run_asciidoc(post)
+  return render_template("posts.html", post = post)
