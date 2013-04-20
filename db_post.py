4d8669954e660f13062118149c4ccc5b62064bbd
Patrick Huck <phuck@lbl.gov>
2013-04-20 00:47:36 -0700

    db_post.py: update timestamp if already posted

--- db_post.py
+++ db_post.py
@@ -8,16 +8,22 @@ from sqlalchemy.orm.exc import NoResultFound
 
 def post(title, ascfile):
   try:
-    Post.query.filter(Post.body == ascfile).one()
+    pq = Post.query.filter(Post.body == ascfile)
+    pq.one()
   except NoResultFound:
-    p = Post(title = title, body = ascfile,
+    post = Post(title = title, body = ascfile,
              timestamp = datetime.datetime.utcnow()
             )
-    db.session.add(p)
-    db.session.commit()
-    print Post.query.get(p.id)
+    print "add %r" % post
+    db.session.add(post)
+    print Post.query.get(post.id)
   else:
     print "%r already posted" % ascfile
+    print "updating timestamp ..."
+    for p in pq.all():
+      p.timestamp = datetime.datetime.utcnow()
+      print Post.query.get(p.id)
+  db.session.commit()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
