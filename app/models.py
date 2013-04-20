af48b3ef315ad8b16ffbf939c8918b9e3b041236
Patrick Huck <phuck@lbl.gov>
2013-04-19 23:55:16 -0700

    models.py: unique body

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   title = db.Column(db.String(120))
-  body = db.Column(db.String(1000))
+  body = db.Column(db.String(1000), unique=True)
   timestamp = db.Column(db.DateTime)
   def __repr__(self):
-    return '<Id %r, Title %r, Post %r>' % (self.id, self.title, self.body)
+    return '<Id %r, Title %r, Body %r>' % (self.id, self.title, self.body)
