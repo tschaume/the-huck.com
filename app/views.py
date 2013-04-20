46cc2ed9705cbce1139a0b201fd0fd2f8764267c
Patrick Huck <phuck@lbl.gov>
2013-04-19 23:55:31 -0700

    views.py: remove fake posts

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from app import app, db
 from app.models import Post
-import datetime
 import StringIO
 from asciidocapi import AsciiDocAPI
 asciidoc = AsciiDocAPI()
@@ -11,19 +10,6 @@ asciidoc.options('--no-header-footer')
 @app.route('/index')
 @app.route('/index/<int:page>')
 def index(page = 1):
-  posts = Post.query.all()
-  for p in posts:
-    db.session.delete(p)
-  db.session.commit()
-  p = Post(title = 'first post', body = 'asciidoc/post.txt',
-           timestamp = datetime.datetime.utcnow()
-          )
-  db.session.add(p)
-  p = Post(title = 'second post', body = 'asciidoc/post.txt',
-           timestamp = datetime.datetime.utcnow()
-          )
-  db.session.add(p)
-  db.session.commit()
   posts = Post.query.paginate(page, 1, False) # returns Paginate object
   for p in posts.items:
     outfile = StringIO.StringIO()
